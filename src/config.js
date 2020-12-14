b4bcda57d6dfd76daec659076f2ca28c9ddabeae
Donovan <donovanle18@gmail.com>
2020-12-14 11:11:16 -0600

    fixed cors issue

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-    API_ENDPOINT: "https://cryptic-meadow-22047.herokuapp.com",
+    API_ENDPOINT:
+        "https://cors-anywhere.herokuapp.com/https://cryptic-meadow-22047.herokuapp.com",
 };
