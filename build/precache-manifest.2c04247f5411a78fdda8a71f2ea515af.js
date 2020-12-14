76aeb3a5bd6073cf3bac7150f98185804181fcbb
Donovan <donovanle18@gmail.com>
2020-12-13 20:25:20 -0600

    wrote up more queue functionality

---
+++ build/precache-manifest.2c04247f5411a78fdda8a71f2ea515af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86915a789997f57d138f3fcac2270942",
+    "url": "/index.html"
+  },
+  {
+    "revision": "048c14cd5d574bd9e7c4",
+    "url": "/static/css/main.f5910334.chunk.css"
+  },
+  {
+    "revision": "6bb3b06f368984c86f38",
+    "url": "/static/js/2.e4cb89bb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e4cb89bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048c14cd5d574bd9e7c4",
+    "url": "/static/js/main.68aafa38.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "db98e777537582a073cec8579d65f6cd",
+    "url": "/static/media/landingpagecats.db98e777.jpg"
+  },
+  {
+    "revision": "ed47dab379a546742bd2099d087a19d6",
+    "url": "/static/media/landingpagedog.ed47dab3.png"
+  }
+]);
