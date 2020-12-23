2d381ba8527d4104e2b16aef7110363f483117de
Donovan <donovanle18@gmail.com>
2020-12-23 11:44:46 -0600

    updated api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    XXX_ENDPOINT:
+    API_ENDPOINT:
         "https://cors-anywhere.herokuapp.com/https://cryptic-meadow-22047.herokuapp.com",
-    API_ENDPOINT: "http://localhost:8080",
+    xxx_ENDPOINT: "http://localhost:8080",
 };
